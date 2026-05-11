5123ef4d634c7d849c4324ddf95f119adc324299
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-11 21:49:03 +0300

    add task3

---
+++ hw1task3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+namespace hw1task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            decimal salary;
+            var kpi = 0;
+            var incomeTax = 13;
+            var pensionFund = 1;
+            decimal issued;
+            bool isNumber;
+            var msg = string.Empty;
+            var bonus = 0;
+            decimal totalSalary = 0;
+
+            Console.WriteLine("Enter employee name");
+            name = Console.ReadLine();
+            if (Regex.IsMatch(name, @"^[a-zA-Z]+$") == false)
+            {
+                Console.WriteLine("Name is not write!");
+            }
+            Console.WriteLine("Enter salary");
+            msg = Console.ReadLine();
+            if (isNumber = decimal.TryParse(msg, out salary))
+            {
+                Console.WriteLine($"Your salary: {salary}");
+
+            }
+            else
+            {
+                Console.WriteLine("Error: In is not a number!");
+            }
+
+            Console.WriteLine("Enter kpi");
+            msg = Console.ReadLine();
+            if (isNumber = int.TryParse(msg, out kpi))
+            {
+                Console.WriteLine($"Your kpi: {kpi}");
+
+            }
+            else
+            {
+                Console.WriteLine("Error: In is not a number!");
+            }
+
+            if (kpi >= 90)
+            {
+                bonus = 20;
+            }
+            else if (kpi >= 75)
+            {
+                bonus = 10;
+            }
+            else
+            {
+                bonus = 0;
+            }
+            totalSalary = salary + (salary * bonus / 100);
+
+            Console.WriteLine($"Your issued: {(totalSalary) - ((totalSalary) * incomeTax / 100) - ((totalSalary) * pensionFund / 100)}");
+
+
+        }
+    }
+}
