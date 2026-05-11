fa53b0c457e1c0fa6ecd6f8e5aea784de91cad8d
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-11 20:55:02 +0300

    add task2

--- hw1task2/Program.cs
+++ hw1task2/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var age = 0;
+bool isNumber;
+Console.WriteLine("Enter age");
+var msg = Console.ReadLine();
+if (isNumber = int.TryParse(msg, out age))
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("You are already an adult");
+
+    }
+    else
+    {
+        Console.WriteLine("You are not an adult");
+    }
+    ;
+}
+else {
+    Console.WriteLine("Error: In is not a number!");
+}
+
