3dabf06121dfb90e04d65ff501413b2d458d93ed
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-15 14:11:35 +0300

    add hw3  task 3

---
+++ hw3task3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace hw3task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctLogin = "admin";
+            string correctPassword = "12345";
+
+            int attempts = 3;
+
+            while (attempts > 0)
+            {
+                Console.Write("Enter login: ");
+                string login = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (login == correctLogin && password == correctPassword)
+                {
+                    Console.WriteLine("Allowed");
+                    return;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"Incorrect login or password. Attempts left: {attempts}");
+                }
+            }
+
+            Console.WriteLine("Denied");
+        }
+    }
+}
