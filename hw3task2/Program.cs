0b9365bffaef30f3ec7b229c74c5dd815d9a3424
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-15 14:04:17 +0300

    add hw3task2

---
+++ hw3task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace hw3task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter score: ");
+            string msg = Console.ReadLine();
+
+            if (int.TryParse(msg, out int score))
+            {
+                string grade = score switch
+                {
+                    >= 90 => "A",
+                    >= 80 => "B",
+                    >= 70 => "C",
+                    >= 60 => "D",
+                    _ => "F"
+
+                };
+
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
+
