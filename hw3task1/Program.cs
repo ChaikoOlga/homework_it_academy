1572785768d5f4861f2a5383291ced04087ae98e
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-15 13:44:45 +0300

    add hw3 task1

---
+++ hw3task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace hw3task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+            var msg = Console.ReadLine();
+            string text = msg;
+            string vowels = "euoaiEUOAI";
+
+            int count = 0;
+
+            foreach (char c in text)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Number of vovels: {count}");
+
+        }
+    }
+}
