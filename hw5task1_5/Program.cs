ac883bb426c1cb793c0becc54e447764f3878d59
Olga Chaiko <chaiko.olga@gmail.com>
2026-06-16 14:18:38 +0300

    add hw5

---
+++ hw5task1_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hw5task1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            int[] data = { 3, 10, -1, 7 };
+            int sum = 0;
+
+            foreach (int number in data)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine("Summ = " + sum);
+        }
+    }
+}
---
+++ hw5task4/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw5task4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> lines = new List<string>()
+        {
+            "One", "Two", "Three", "Four", "Five",
+            "Six", "Seven", "Eight", "Nine", "Ten",
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+            "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"
+        };
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string line in lines)
+            {
+                sb.Append(line);
+            }
+
+            string text = sb.ToString();
+
+            Console.WriteLine(text.Length);
+        }
+    }
+}
