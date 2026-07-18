7979277ca8e8645e4b6576fbd3cfea51e36d5535
Olga Chaiko <chaiko.olga@gmail.com>
2026-07-18 23:12:21 +0300

    add hw 12

---
+++ hw12/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace hw12
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ApiService service = new ApiService();
+
+            Console.WriteLine("Task 1");
+
+            await foreach (Product product in service.GetProductsAsync())
+            {
+                Console.WriteLine(product.Id + " " + product.Name);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Task 2");
+
+            CancellationTokenSource source =
+                new CancellationTokenSource();
+
+            source.CancelAfter(3000);
+
+            try
+            {
+                await service.LongRequestAsync(source.Token);
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Request cancelled");
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
