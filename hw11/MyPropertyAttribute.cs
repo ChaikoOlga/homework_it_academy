7a5c57a306429782b96bafe368cc98a533c48125
Olga Chaiko <chaiko.olga@gmail.com>
2026-07-18 17:02:22 +0300

    add hw 11

---
+++ hw11/MyPropertyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw11
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MyPropertyAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public MyPropertyAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
